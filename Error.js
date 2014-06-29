a508c2557c46e408cee0a6e1ca681ea9d6ffab83
Josh Terrell <joshterrell805@gmail.com>
2014-06-28 17:01:57 -0700

    Add todo, add new error to Error.js

--- Error.js
+++ Error.js
@@ -45,3 +45,14 @@ function LogicError() {
 NotReadyError.prototype = Object.create(ExtendableError.prototype);
 NotReadyError.prototype.name = 'NotReadyError';
 NotReadyError.prototype.constructor = NotReadyError;
+
+/**
+ * Throw if a piece of functionality isn't implemented yet but it was used
+ *  anyway.
+ */
+function NotImplementedError() {
+   ExtendableError.apply(this, arguments);
+}
+NotImplementedError.prototype = Object.create(ExtendableError.prototype);
+NotImplementedError.prototype.name = 'NotImplementedError';
+NotImplementedError.prototype.constructor = NotImplementedError;
